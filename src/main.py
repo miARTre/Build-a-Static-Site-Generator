b317a536be274eb1bf353632c157d57fa6d7bdb6
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-01-23 22:23:49 +0100

    Add main.py with TextNode demonstration

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    new_textNode = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(new_textNode)
+
+if __name__ == "__main__":
+    main()
