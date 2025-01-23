5a8761b626fc32040574decbf59eedfca2b73d6f
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-01-24 00:53:43 +0100

    test: add HTMLNode test cases for props and initialization

---
+++ src/test_htmlnode.py
@@ -0,0 +1,57 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode(props={"href": "https://google.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), ' href="https://google.com" target="_blank"')
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_initialization(self):
+        node = HTMLNode(tag="p", value="Hello", children=[], props={"class": "text"})
+        self.assertEqual(node.tag, "p")
+        self.assertEqual(node.value, "Hello")
+        self.assertEqual(node.children, [])
+        self.assertEqual(node.props, {"class": "text"})
+
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
+
+    
